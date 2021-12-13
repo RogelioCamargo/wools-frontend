7d00fad4b89013d0c8089b2e8ad2e91a28396ae3
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-12-13 09:31:32 -0800

    Install react router and make routes to main pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Col } from "react-bootstrap";
 
 // components
 import LeftNavigationBar from "./components/LeftNavigationBar";
@@ -12,16 +13,17 @@ import Supplies from "./pages/Supplies";
 function App() {
   return (
     <div>
-      <div style={{ height: "80px", backgroundColor: "blue" }}>
-      </div>
+      <div style={{ height: "80px", backgroundColor: "blue" }}></div>
       <div style={{ display: "flex" }}>
         <Col xs={3}>
           <LeftNavigationBar />
         </Col>
         <Col>
-          <Dashboard />
-          <Inventory />
-          <Supplies />
+          <Routes>
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/supplies" element={<Supplies />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
         </Col>
       </div>
     </div>
--- src/components/LeftNavigationBar.js
+++ src/components/LeftNavigationBar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LeftNavigationBar = () => {
 	return (
-		<div className="container">
-			<p>Dashboard</p>
-			<p>Inventory</p>
-			<p>Supplies</p>
-		</div>
-	);
+    <div className="container">
+      <Link to="/">Dashboard</Link>
+      <br />
+      <Link to="/inventory">Inventory</Link>
+      <br />
+      <Link to="/supplies">Supplies</Link>
+    </div>
+  );
 };
 
 export default LeftNavigationBar;
