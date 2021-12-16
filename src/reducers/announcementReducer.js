40b7d307fff762ea21cb371d3554da58287c4d17
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-12-15 18:58:26 -0800

    Create reducers

---
+++ src/reducers/announcementReducer.js
@@ -0,0 +1,82 @@
+import itemService from "../services/itemService";
+
+const url = "http://localhost:3001/api/messages";
+
+const announcementReducer = (state = [], action) => {
+  switch (action.type) {
+    case "INITIALIZE_ANNOUNCEMENTS":
+      return action.payload;
+    case "CREATE_ANNOUNCEMENT":
+      return [...state, action.payload];
+    case "UPDATE_ANNOUNCEMENT":
+      return state.map((blog) => {
+        if (blog.id === action.payload.id) return action.payload;
+        else return blog;
+      });
+    case "DELETE_ANNOUNCEMENT":
+      return state.filter((blog) => blog.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export const initializeAnnouncements = () => {
+  return async (dispatch) => {
+    try {
+      const items = await itemService.getAll(`${url}/?type=announcement`);
+      dispatch({
+        type: "INITIALIZE_ANNOUNCEMENTS",
+        payload: items,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const createOneAnnouncement = (message) => {
+  return async (dispatch) => {
+    try {
+      const newMessage = await itemService.createOne(url, message);
+      dispatch({
+        type: "CREATE_ANNOUNCEMENT",
+        payload: newMessage,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const updateOneAnnouncement = (message) => {
+  return async (dispatch) => {
+    try {
+      const updatedMessage = await itemService.updateOne(
+        `${url}/${message.id}`,
+        message
+      );
+      dispatch({
+        type: "UPDATE_ANNOUNCEMENT",
+        payload: updatedMessage,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const deleteOneAnnouncement = (id) => {
+  return async (dispatch) => {
+    try {
+      await itemService.deleteOne(`${url}/${id}`, id);
+      dispatch({
+        type: "DELETE_ANNOUNCEMENT",
+        payload: id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export default announcementReducer;
---
+++ src/reducers/ticketReducer.js
@@ -0,0 +1,82 @@
+import itemService from "../services/itemService";
+
+const url = "http://localhost:3001/api/messages";
+
+const ticketReducer = (state = [], action) => {
+  switch (action.type) {
+    case "INITIALIZE_TICKETS":
+      return action.payload;
+    case "CREATE_TICKET":
+      return [...state, action.payload];
+    case "UPDATE_TICKET":
+      return state.map((blog) => {
+        if (blog.id === action.payload.id) return action.payload;
+        else return blog;
+      });
+    case "DELETE_TICKET":
+      return state.filter((blog) => blog.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export const initializeTickets = () => {
+  return async (dispatch) => {
+		try {
+			const items = await itemService.getAll(`${url}/?type=ticket`);
+			dispatch({
+				type: "INITIALIZE_TICKETS",
+				payload: items,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+  };
+};
+
+export const createOneTicket = (message) => {
+  return async (dispatch) => {
+		try {
+			const newMessage = await itemService.createOne(url, message);
+			dispatch({
+				type: "CREATE_TICKET",
+				payload: newMessage,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+  };
+};
+
+export const updateOneTicket = (message) => {
+  return async (dispatch) => {
+		try {
+			const updatedMessage = await itemService.updateOne(
+				`${url}/${message.id}`,
+				message
+			);
+			dispatch({
+				type: "UPDATE_TICKET",
+				payload: updatedMessage,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+  };
+};
+
+export const deleteOneTicket = (id) => {
+  return async (dispatch) => {
+		try {
+			await itemService.deleteOne(`${url}/${id}`, id);
+			dispatch({
+				type: "DELETE_TICKET",
+				payload: id,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+  };
+};
+
+export default ticketReducer;
