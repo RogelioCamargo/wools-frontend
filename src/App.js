40f8948f70fdee257c06205a328a00db1f1985c6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-04 08:31:47 -0800

    Create a catchall route for router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 // components
 import LeftNavigationBar from "./components/LeftNavigationBar";
@@ -23,6 +23,7 @@ function App() {
             <Route path="/inventory" element={<Inventory />} />
             <Route path="/supplies" element={<Supplies />} />
             <Route path="/" element={<Dashboard />} />
+						<Route path="*" element={<Navigate to="/"/>} />
           </Routes>
         </main>
       </div>
